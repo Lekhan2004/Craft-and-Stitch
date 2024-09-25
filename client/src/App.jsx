d185e74bc245fa079bac8c1c8bc36565641487fb
Lekhan2004 <luckylekhan25@gmail.com>
2024-09-25 14:05:18 +0530

    Added Vite+React Template for C&S

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-xl font-bold text-center'>
+        Hi Welcome to C&S - The place where Producers and Consumers meet
+      </div>
+    </>
+  )
+}
+
+export default App
